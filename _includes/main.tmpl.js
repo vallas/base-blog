ca2d57c652c7002eba1fa988da612fff5a810e60
vallas <31855344+vallas@users.noreply.github.com>
2022-12-06 14:13:24 +0100

    Add files via upload

---
+++ _includes/main.tmpl.js
@@ -0,0 +1,14 @@
+export default ()=>
+    console.log("hello")
+
+// https://lume.land/plugins/modules/#creating-layouts
+// https://dev.to/j471n/detect-when-users-switch-tabs-using-javascript-3mi3
+
+// when the uer loses focus
+window.addEventListener("blur", () => {
+    decodeURIComponent.title = "Breakup";
+});
+
+window.addEventListener("focus", () => {
+    decodeURIComponent.title = "Patch Up";
+});
